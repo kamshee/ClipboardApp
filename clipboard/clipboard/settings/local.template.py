9ab0a8e6933722447f2e6f812b6e69f8749ef237
mirrorecho <info@randallwest.com>
2018-03-20 15:18:16 -0400

    local vs base settings

---
+++ clipboard/clipboard/settings/local.template.py
@@ -0,0 +1,12 @@
+# INSTRUCTIONS:
+# copy contents from this file to "local.py" (which is not in source control)
+# replace SECRET_KEY with unique value (e.g. use https://djskgen.herokuapp.com/)
+
+from clipboard.settings.base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# replace with local scret key: 
+SECRET_KEY = 'REPLACE_ME!!!!!!!'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
